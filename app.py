e7fbeeeb017da1d86cfd4d03f164bb2e06e7e4f4
divya.sethi <divya.sethi@gdn-commerce.com>
2017-12-20 18:11:41 +0530

    stable code1

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import logging
 
 
 app = Flask(__name__)
-LOG = logging.getLogger(__name__)
-LOG.setLevel(logging.DEBUG)
+# LOG = logging.getLogger("Server")
+# LOG.setLevel(logging.DEBUG)
 query = ""
 
 
@@ -16,15 +16,15 @@ def inputKeyword():
 @app.route('/process', methods=['POST'])
 def display():
 	global query
-	LOG.info("got request to process: " + request.form['query'])
+	print("got request to process: " + request.form['query'])
 	if request.form['query'] == query:
-		LOG.debug("got the same request as the previous one, current request: " 
+		print("got the same request as the previous one, current request: " 
 			+ request.form['query'] + " previous request: " + query)
 		return render_template('input.html', message="Your request is proccessed")
 	query = request.form['query']
-	LOG.debug("processing request: " + query)
+	print("processing request: " + query)
 	processStream(request.form['query'])
-	LOG.debug("returning from process method")
+	print("returning from process method")
 	return render_template('input.html', message="Your request is proccessed")
 
 if __name__ == "__main__":
--- tweets.py
+++ tweets.py
@@ -8,62 +8,21 @@ import logging
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch([{"host": "localhost", "port": 9200}])
-LOG = logging.getLogger(__name__)
+LOG = logging.getLogger("tweets")
 LOG.setLevel(logging.DEBUG)
 ELASTICSEARCH_INDEX = "twitter"
 ELASTICSEARCH_TYPE = "tweet"
 INDEX_BODY_PATH = "resources/index_settings_mappings.json"
 
-
-consumer_key = 'UZ4u0kuF5i4uRXcxSk5Iv8ZnJ'
-consumer_secret = 'wLMtGwt8WxMWgqpipU28xOYPTrTW72znEQsCgS801XTXyYZIqU'
-access_token = '183456396-hzR4O5iUBVw6lZMsUPzbFi6wg6fYgf77Mbt7ZZvr'
-access_secret = 'nXLmRoQ0XCM4j3e7tKrQzQyt88UpKxWrrTDOxO4iyEh5t'
-kafka = KafkaClient("172.16.26.105:9092")
-producer = SimpleProducer(kafka)
-
 flag = False
 query = ""
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
-	
-api = tweepy.API(auth)
-twitter_stream = Stream(auth, MyListener())
-
-def pollCurrentStreaming():
-	global flag
-	while True:
-		if flag is True:
-			if threadStream.isAlive() is False:
-				threadStream.start()
-			else:
-				if twitter_stream.running is True:
-					twitter_stream.disconnect
-				threadStream.stop()
-				threadStream.start()
-				flag = False
-
-def processStream(input_query):
-	global query, flag 
-	query = input_query
-	flag = True
-	if threadPoll.isAlive() is False:
-		threadPoll.start();
-
-
-def startStreaming():
-	global twitter_stream
-	if twitter_stream.running is True:
-		twitter_stream.disconnect
-		twitter_stream.filter(track=query.split(","))
-
 
 class MyListener(StreamListener):
 
 	def on_data(self, data):
 		try:
 			producer.send_messages("twitter", data.encode('utf-8'))
-			print(data, type(data))
+			print(data)
 			return True
 		except BaseException as e:
 			print("Error on_data: %s" % str(e))
@@ -73,11 +32,66 @@ class MyListener(StreamListener):
 		print(status)
 		return True    
 
-threadPoll = Thread(target= pollCurrentStreaming)
-threadStream = Thread(target = startStreaming)
+class ThreadStreaming(Thread):
+	flagStartStreaming = False;
+	def __init__(self):
+		super(ThreadStreaming, self).__init__()
+	def startStreaming(self):
+		global twitter_stream, flag
+		self.flagStartStreaming = False
+		twitter_stream.filter(track=query.split(","))
+	def run (self):
+		while True:
+			if self.flagStartStreaming is True:
+				self.startStreaming()
+	def setFlagStartStreaming(self, var):
+		self.flagStartStreaming = var	
+
+threadStream = ThreadStreaming()	
+
+class ThreadPoll(Thread):
+	def __init__(self):
+		super(ThreadPoll, self).__init__()
+	def stopStreaming(self):
+		global twitter_stream
+		if twitter_stream.running is True:
+			twitter_stream.disconnect() 		
+	def run(self):
+		global threadStream, flag
+		while True:
+			if flag is True:
+				self.stopStreaming()
+				flag = False
+				threadStream.setFlagStartStreaming(True)
+threadPoll = ThreadPoll()
+
+consumer_key = 'UZ4u0kuF5i4uRXcxSk5Iv8ZnJ'
+consumer_secret = 'wLMtGwt8WxMWgqpipU28xOYPTrTW72znEQsCgS801XTXyYZIqU'
+access_token = '183456396-hzR4O5iUBVw6lZMsUPzbFi6wg6fYgf77Mbt7ZZvr'
+access_secret = 'nXLmRoQ0XCM4j3e7tKrQzQyt88UpKxWrrTDOxO4iyEh5t'
+kafka = KafkaClient("172.16.26.105:9092")
+producer = SimpleProducer(kafka)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+	
+api = tweepy.API(auth)
+twitter_stream = Stream(auth, MyListener())
+print("Tweets: everything initialised")
+def processStream(input_query):
+	global query, flag , threadPoll
+	print("processing stream: " + input_query)
+	query = input_query
+	flag = True
+	if threadPoll.is_alive() is False:
+		print("starting threadpoll")
+		threadPoll.start()
+	if threadStream.is_alive() is False:
+		print("starting threadStream")
+		threadStream.start()
 
 def recreateIndex():
-    LOG.info("recreating index : " + ELASTICSEARCH_INDEX + " with file: " + INDEX_BODY_PATH)
+    print("recreating index : " + ELASTICSEARCH_INDEX + " with file: " + INDEX_BODY_PATH)
     es.indices.delete(index= ELASTICSEARCH_INDEX)
     with open(INDEX_BODY_PATH, "r") as f:
         index_body = f.read()
